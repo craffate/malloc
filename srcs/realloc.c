5a2330e3e7a38edb9b0313b9ebf5ea0c5b769024
Cyril Raffatelli <craffate@student.42.fr>
2020-06-30 19:39:36 +0200

    Edit realloc

--- srcs/realloc.c
+++ srcs/realloc.c
@@ -6,7 +6,7 @@
 /*   By: craffate <craffate@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/30 11:35:42 by craffate          #+#    #+#             */
-/*   Updated: 2020/06/30 11:52:54 by craffate         ###   ########.fr       */
+/*   Updated: 2020/06/30 19:33:34 by craffate         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void					*realloc(void *ptr, size_t size)
 	void				*p;
 	size_t				ptr_size;
 
-	ptr_size = (size_t)*(((char *)ptr) - ((char)sizeof(size_t)));
+	ptr_size = ((t_chunk *)((char *)ptr - sizeof(t_chunk)))->size;
 	free(ptr);
 	p = malloc(size);
 	p = ft_memcpy(p, ptr, ptr_size);
