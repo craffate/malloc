87b80fffc08002ce550c796febeacde804fed356
Cyril Raffatelli <craffate@student.42.fr>
2020-06-30 11:53:52 +0200

    Add realloc data copy

--- srcs/realloc.c
+++ srcs/realloc.c
@@ -6,7 +6,7 @@
 /*   By: craffate <craffate@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/30 11:35:42 by craffate          #+#    #+#             */
-/*   Updated: 2020/06/30 11:36:22 by craffate         ###   ########.fr       */
+/*   Updated: 2020/06/30 11:52:54 by craffate         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,11 @@
 void					*realloc(void *ptr, size_t size)
 {
 	void				*p;
+	size_t				ptr_size;
 
+	ptr_size = (size_t)*(((char *)ptr) - ((char)sizeof(size_t)));
 	free(ptr);
 	p = malloc(size);
+	p = ft_memcpy(p, ptr, ptr_size);
 	return (p);
 }
