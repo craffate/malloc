e1202d69b29ec918b75b7cb8d5befdc2e6be6dbb
Cyril Raffatelli <craffate@student.42.fr>
2020-07-01 11:01:46 +0200

    Add range check for realloc

--- srcs/realloc.c
+++ srcs/realloc.c
@@ -6,20 +6,52 @@
 /*   By: craffate <craffate@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/30 11:35:42 by craffate          #+#    #+#             */
-/*   Updated: 2020/06/30 19:33:34 by craffate         ###   ########.fr       */
+/*   Updated: 2020/07/01 10:55:09 by craffate         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "malloc.h"
 
+static int				check_range(void *ptr)
+{
+	int					ret;
+	uintptr_t			ptr_idx;
+	t_page				*idx;
+
+	ret = 0;
+	ptr_idx = (uintptr_t)ptr;
+	idx = g_arena->tiny;
+	while (idx && !ret)
+	{
+		if ((uintptr_t)idx < ptr_idx && (uintptr_t)((char *)idx + idx->size) > ptr_idx)
+			ret = 1;
+		idx = idx->next;
+	}
+	idx = g_arena->small;
+	while (idx && !ret)
+	{
+		if ((uintptr_t)idx < ptr_idx && (uintptr_t)((char *)idx + idx->size) > ptr_idx)
+			ret = 1;
+		idx = idx->next;
+	}
+	idx = g_arena->large;
+	while (idx && !ret)
+	{
+		if ((uintptr_t)idx < ptr_idx && (uintptr_t)((char *)idx + idx->size) > ptr_idx)
+			ret = 1;
+		idx = idx->next;
+	}
+	return (ret);
+}
+
 void					*realloc(void *ptr, size_t size)
 {
 	void				*p;
-	size_t				ptr_size;
 
-	ptr_size = ((t_chunk *)((char *)ptr - sizeof(t_chunk)))->size;
+	p = NULL;
 	free(ptr);
 	p = malloc(size);
-	p = ft_memcpy(p, ptr, ptr_size);
+	if (ptr && check_range(ptr))
+		ft_memcpy(p, ptr, size);
 	return (p);
 }
