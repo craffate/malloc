6dbc98c3f38ac6c07826eae92f90b2099dac21b2
Cyril Raffatelli <craffate@student.42.fr>
2020-07-22 06:02:21 +0200

    Fix realloc crashes

--- srcs/realloc.c
+++ srcs/realloc.c
@@ -6,7 +6,7 @@
 /*   By: craffate <craffate@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/30 11:35:42 by craffate          #+#    #+#             */
-/*   Updated: 2020/07/01 10:55:09 by craffate         ###   ########.fr       */
+/*   Updated: 2020/07/22 06:01:52 by craffate         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,27 @@ static int				check_range(void *ptr)
 	return (ret);
 }
 
+static void				copy_data(char *dst, char *src)
+{
+	unsigned int		idx;
+	size_t				dst_size;
+	size_t				src_size;
+
+	idx = -1u;
+	dst_size = (size_t)((t_chunk *)(dst - sizeof(t_chunk)))->size;
+	src_size = (size_t)((t_chunk *)(src - sizeof(t_chunk)))->size;
+	if (dst_size < src_size)
+	{
+		while (++idx < dst_size)
+			dst[idx] = src[idx];
+	}
+	else
+	{
+		while (++idx < src_size)
+			dst[idx] = src[idx];
+	}
+}
+
 void					*realloc(void *ptr, size_t size)
 {
 	void				*p;
@@ -52,6 +73,6 @@ void					*realloc(void *ptr, size_t size)
 	free(ptr);
 	p = malloc(size);
 	if (ptr && check_range(ptr))
-		ft_memcpy(p, ptr, size);
+		copy_data(p, ptr);
 	return (p);
 }
